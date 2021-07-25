41edb079928c163556dd770de936d659e649a945
Cong Nguyen Thanh <thanhcong.bk10@gmail.com>
2021-07-25 16:42:40 +0700

    implement K-way merged

--- main.cpp
+++ main.cpp
@@ -32,115 +32,6 @@ inline bool wrapper_lexicographical_compare_1(const pair<int, string>& s1, const
 #define MAX_IN_BUFFER_SIZE 2
 #define MAX_OUT_BUFFER_SIZE 2
 
-
-std::deque<vector<string>> inBuffer;
-std::deque<vector<string>> outBuffer;
-std::mutex in_mutex;
-std::mutex out_mutex;
-std::condition_variable in_cv;
-std::condition_variable out_cv;
-bool reader_finished = false;
-bool sorter_finished = false;
-
-void FileReader(const string& inputFile, long heapMemLimit){
-    ifstream inF(inputFile, ifstream::in);
-    long maxBatchSize = heapMemLimit/(2*MAX_IN_BUFFER_SIZE+2);
-    while (inF.good()){
-        vector<string> buffer;
-        long heapMemConsume = 0;
-
-        //Load
-        while (inF.good() && heapMemConsume < maxBatchSize){
-            string line;
-            getline(inF, line);
-            heapMemConsume += line.size();
-            buffer.push_back(move(line));
-        }
-        if (buffer.back().empty()){ //TODO
-            buffer.pop_back();
-        }
-
-        {
-            std::unique_lock<std::mutex> in_lock(in_mutex);
-            //When the queue is full, it returns false, it has been blocked in this line
-            in_cv.wait(in_lock, [] {return inBuffer.size() < MAX_IN_BUFFER_SIZE; });
-            inBuffer.push_front(move(buffer));
-            reader_finished = inF.good() ? false : true;
-        }
-        in_cv.notify_one();
-    }
-    cout<<"FileReader Finished"<<endl;
-}
-
-void Sorter(){
-    bool isDone = false;
-    while (!isDone) {
-        vector<string> buffer;
-        {
-            std::unique_lock<std::mutex> in_lock(in_mutex);
-            //When the queue is empty, it returns false, it has been blocked in this line
-            in_cv.wait(in_lock, [] {return !inBuffer.empty(); });
-            swap(buffer, inBuffer.back());
-            inBuffer.pop_back();
-            isDone = (reader_finished && (inBuffer.size() == 0));
-        }
-        in_cv.notify_one();
-
-        //Sort
-        sort(buffer.begin(), buffer.end(), wrapper_lexicographical_compare);
-        //To Do: Try lock and add to internal buffer
-
-        {
-            std::unique_lock<std::mutex> out_lock(out_mutex);
-            //When the queue is full, it returns false, it has been blocked in this line
-            out_cv.wait(out_lock, [] {return outBuffer.size() < MAX_OUT_BUFFER_SIZE; });
-            outBuffer.push_front(move(buffer));
-            sorter_finished = isDone;
-        }
-        out_cv.notify_one();
-    }
-    cout<<"Sorter Finished"<<endl;
-}
-
-void FileWriter(const string& inputFile, int& numRun){
-    bool isDone = false;
-    numRun = 0;
-    int numLine = 0;
-    while(!isDone) {
-        vector<string> buffer;
-        {
-            std::unique_lock<std::mutex> out_lock(out_mutex);
-            //When the queue is full, it returns false, it has been blocked in this line
-            out_cv.wait(out_lock, [] {return !outBuffer.empty(); });
-            swap(buffer, outBuffer.back());
-            outBuffer.pop_back();
-            isDone = (sorter_finished && (outBuffer.size() == 0));
-        }
-        out_cv.notify_one();
-        //Store
-        ofstream outF(inputFile + "_" + to_string(numRun), ofstream::out);
-        for(auto& x : buffer){
-            outF<<x<<endl;
-            numLine++;
-        }
-        outF.close();
-        numRun++;
-    }
-    cout<<"FileWriter Finished, numline = "<<numLine<<endl;
-}
-
-int InitialPhase(const string& inputFile, long heapMemLimit){
-    int numRun = 0;
-    std::thread t1(FileReader, inputFile, heapMemLimit);
-	std::thread t2(Sorter);
-    std::thread t3(FileWriter, inputFile, std::ref(numRun));
-	t1.join();
-	t2.join();
-    t3.join();
-    return numRun;
-}
-
-
 /*************/
 struct mmap_info{
     int fd;
@@ -154,6 +45,7 @@ bool open_mmap(mmap_info &info, const std::string& fileName, int flag, off_t siz
     // flag: https://man7.org/linux/man-pages/man2/open.2.html
     info.fd = open(fileName.c_str(), flag);
     if (info.fd == -1) {
+        cout<<"Can't open file descriptor of"<<fileName<<endl;
         return false;
     }
     if (size < 0){
@@ -167,6 +59,7 @@ bool open_mmap(mmap_info &info, const std::string& fileName, int flag, off_t siz
     
     if (info.beginPos == MAP_FAILED) {
         close(info.fd);
+        cout<<"Can't open mmap of"<<fileName<<endl;
         return false;
     }
     info.endPos = info.beginPos + info.size;
@@ -189,13 +82,153 @@ inline char* readline_mmap(mmap_info &info, int& length){
     return p;
 }
 
-void KWayMerged(int k, const string& inputFile, const string& outputFile, long heapMemLimit){
-    vector<string> outBuffer;
+std::deque<vector<string>> inBuffer;
+std::deque<string> outBuffer;
+std::mutex in_mutex;
+std::mutex out_mutex;
+std::condition_variable in_cv;
+std::condition_variable out_cv;
+bool reader_finished = false;
+bool sorter_finished = false;
+
+void FileReader(const string& inputFile, long heapMemLimit){
+    mmap_info mmapInput;
+    open_mmap(mmapInput, inputFile, O_RDONLY);
+
+    long maxBatchSize = heapMemLimit/(2*MAX_IN_BUFFER_SIZE+2);
+    int lines = 0;
+    while (mmapInput.currentPos){
+        vector<string> buffer;
+        long heapMemConsume = 0;
+
+        //Load
+        while (mmapInput.currentPos && heapMemConsume < maxBatchSize){
+            int length = 0;
+            char* p = readline_mmap(mmapInput, length);
+            heapMemConsume += length;
+            buffer.push_back(string(p, length));
+            lines++;
+        }
+        //cout<<buffer.back();
+
+        {
+            std::unique_lock<std::mutex> in_lock(in_mutex);
+            //When the queue is full, it returns false, it has been blocked in this line
+            in_cv.wait(in_lock, [] {return inBuffer.size() < MAX_IN_BUFFER_SIZE; });
+            inBuffer.push_front(move(buffer));
+            reader_finished = mmapInput.currentPos ? false : true;
+        }
+        in_cv.notify_one();
+    }
+    close_mmap(mmapInput);
+    //cout<<"FileReader Finished lines = "<<lines<<endl;
+}
+
+void Sorter(){
+    int numLine = 0;
+    bool isDone = false;
+    while (!isDone) {
+        vector<string> buffer;
+        {
+            std::unique_lock<std::mutex> in_lock(in_mutex);
+            //When the queue is empty, it returns false, it has been blocked in this line
+            in_cv.wait(in_lock, [] {return !inBuffer.empty(); });
+            swap(buffer, inBuffer.back());
+            inBuffer.pop_back();
+            isDone = (reader_finished && (inBuffer.size() == 0));
+        }
+        in_cv.notify_one();
+
+        //Sort
+        sort(buffer.begin(), buffer.end(), wrapper_lexicographical_compare);
+        //To Do: Try lock and add to internal buffer
+        string tmp;
+        for(auto &x : buffer){ tmp += x; numLine++;}
+        
+        {
+            std::unique_lock<std::mutex> out_lock(out_mutex);
+            //When the queue is full, it returns false, it has been blocked in this line
+            out_cv.wait(out_lock, [] {return outBuffer.size() < MAX_OUT_BUFFER_SIZE; });
+            outBuffer.push_front(move(tmp));
+            sorter_finished = isDone;
+        }
+        out_cv.notify_one();
+    }
+    //cout<<"Sorter Finished, numline = "<<numLine<<endl;
+}
+
+void FileWriter(const string& inputFile, int& numRun){
+    bool isDone = false;
+    numRun = 0;
+    while(!isDone) {
+        string buffer;
+        {
+            std::unique_lock<std::mutex> out_lock(out_mutex);
+            //When the queue is full, it returns false, it has been blocked in this line
+            out_cv.wait(out_lock, [] {return !outBuffer.empty(); });
+            swap(buffer, outBuffer.back());
+            outBuffer.pop_back();
+            isDone = (sorter_finished && (outBuffer.size() == 0));
+        }
+        out_cv.notify_one();
+        //Store
+        FILE * pFile;
+        pFile = fopen ((inputFile + "_0_" + to_string(numRun)).c_str(), "wb");
+        fwrite (buffer.c_str() , sizeof(char), buffer.size(), pFile);
+        fclose (pFile);
+        numRun++;
+    }
+}
+
+int InitialPhase(const string& inputFile, long heapMemLimit){
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    int numRun = 0;
+    std::thread t1(FileReader, inputFile, heapMemLimit);
+	std::thread t2(Sorter);
+    std::thread t3(FileWriter, inputFile, std::ref(numRun));
+	t1.join();
+	t2.join();
+    t3.join();
+    std::deque<vector<string>>().swap(inBuffer);
+    std::deque<string>().swap(outBuffer);
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    std::cout << "InitialPhase Elapsed time = " << std::chrono::duration_cast<std::chrono::seconds>(end - begin).count() << "[s]" << std::endl;
+    return numRun;
+}
+
+queue<string> meregedOutBuffer;
+bool merger_finished = false;
+
+void MergedFileWriter(const string& outputFile){
+    //int block_size = 4096;
+    bool isDone = false;
+    string buffer;
+    FILE * pFile;
+    pFile = fopen (outputFile.c_str(), "wb");
+
+    while(!isDone) {
+        {
+            std::unique_lock<std::mutex> out_lock(out_mutex);
+            //When the queue is full, it returns false, it has been blocked in this line
+            out_cv.wait(out_lock, [] {return !meregedOutBuffer.empty(); });
+            swap(buffer, meregedOutBuffer.back());
+            meregedOutBuffer.pop();
+            isDone = (merger_finished && meregedOutBuffer.empty());
+        }
+        out_cv.notify_one();
+        fwrite (buffer.c_str() , sizeof(char), buffer.size(), pFile);
+    }
+    fclose (pFile);
+    //cout<<"MergedFileWriter Finished"<<endl;
+}
+
+// Merge k files:  ${inputFile}_${step}_${base} -> ${inputFile}_${step}_${base + k}
+void KWayMerged(int k, int base, const string& inputFile, long heapMemLimit){
+    //vector<string> outBuffer;
     vector<mmap_info> mmapInfos(k);
     vector<pair<int, string>> heapLines(k);
     for (int i = 0; i < k; i++){
-        bool canOpen = open_mmap(mmapInfos[i], inputFile + "_" + to_string(i), O_RDONLY);
-        if (!canOpen) cout<<"Can't open "<<inputFile + "_"<<i<<endl;
+        open_mmap(mmapInfos[i], inputFile + "_" + to_string(i + base), O_RDONLY);
 
         int length = 0;
         char* p = readline_mmap(mmapInfos[i], length);
@@ -203,11 +236,12 @@ void KWayMerged(int k, const string& inputFile, const string& outputFile, long h
     }
 
     std::make_heap (heapLines.begin(),heapLines.end(), wrapper_lexicographical_compare_1);
-
+    string buffer;
+    buffer.reserve(heapMemLimit/3);
     while(heapLines.size()) {
         std::pop_heap(heapLines.begin(),heapLines.end(), wrapper_lexicographical_compare_1);
         int mmapIdx = heapLines.back().first;
-        outBuffer.push_back(move(heapLines.back().second));
+        buffer += string(move(heapLines.back().second));
         heapLines.pop_back();
         
         if (mmapInfos[mmapIdx].currentPos) {
@@ -218,22 +252,57 @@ void KWayMerged(int k, const string& inputFile, const string& outputFile, long h
         } else{
             close_mmap(mmapInfos[mmapIdx]);
             //remove temp file
-            //remove( (inputFile + "_" + to_string(mmapIdx)).c_str());
+            remove((inputFile + "_" + to_string(mmapIdx + base)).c_str());
+        }
+
+        if (buffer.size() > heapMemLimit/3 || heapLines.empty()) {
+            {
+                std::unique_lock<std::mutex> out_lock(out_mutex);
+                //When the queue is full, it returns false, it has been blocked in this line
+                out_cv.wait(out_lock, [] {return meregedOutBuffer.size() < MAX_OUT_BUFFER_SIZE; });
+                // Block in here
+                meregedOutBuffer.push(buffer);
+                buffer.clear();
+                merger_finished = heapLines.empty();
+            }
+            out_cv.notify_one();
         }
     }
-
-    int numLine = 0;
-    ofstream outF(outputFile, ofstream::out);
-    for(auto& x : outBuffer){
-        outF<<x;
-        numLine++;
-    }
-    cout<<"numLine = "<<numLine<<endl;
-    outF.close();
+    //cout<<"KWayMerged Finished"<<endl;
 }
 
 void MergedPhase(const string& inputFile, const string& outputFile, int numRun, long heapMemLimit){
-    KWayMerged(numRun, inputFile, outputFile, heapMemLimit);
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    int step = 0;
+    int base = 0;
+    int numRunStep = 0; // number run at this step
+    string outputMergedFile;
+    while (numRun > 1){
+        int k = numRun - base > 100 ? 100 : numRun - base; //To do
+        if (k < numRun){
+            outputMergedFile = inputFile + "_" + to_string(step+1) + "_" + to_string(numRunStep);
+        } else {
+            outputMergedFile = outputFile;
+        }
+
+        //cout<<outputMergedFile<<" k = "<<k<<" step = "<<step<<" base = "<<base<<endl;
+
+        std::thread t1(KWayMerged, k, base, inputFile + "_" + to_string(step), heapMemLimit);
+        std::thread t2(MergedFileWriter, outputMergedFile);
+        t1.join();
+        t2.join();
+        queue<string>().swap(meregedOutBuffer);
+        base += k;
+        numRunStep++;
+        if (base >= numRun){
+            base = 0;
+            numRun = numRunStep;
+            numRunStep = 0;
+            step++;
+        }
+    }
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    std::cout << "MergedPhase Elapsed time = " << std::chrono::duration_cast<std::chrono::seconds>(end - begin).count() << "[s]" << std::endl;
 }
 
 int main(int argc, char **argv)
--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 BASEDIR=$(dirname "$0")
-${BASEDIR}/main.out ${BASEDIR}/testcase/world192.txt ${BASEDIR}/testcase/world192_out.txt 16384
+#${BASEDIR}/main.out ${BASEDIR}/testcase/world192_100.txt ${BASEDIR}/testcase/world192_100_out.txt 8388608
+#${BASEDIR}/main.out ${BASEDIR}/testcase/world192.txt ${BASEDIR}/testcase/world192_out.txt 83886
+${BASEDIR}/main.out ${BASEDIR}/testcase/world192_1000.txt ${BASEDIR}/testcase/world192_1000_out.txt 104857600
